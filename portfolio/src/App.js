0f6c5ad1db0d15fd992990fed966edb25898b1d2
joao vitor de lima gonçalves <88281625+joaolima16@users.noreply.github.com>
2023-04-10 14:14:53 -0300

    Add files via upload

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AboutMe from './Components/AboutMeSection';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AboutMe />
+
+    </div>
+  );
+}
+
+export default App;
